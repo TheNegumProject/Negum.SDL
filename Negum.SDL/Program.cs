44e505779694eaabb72d39451a54c13c268d41a3
kdobrzynski <kdobrzynski@pl.imshealth.com>
2021-04-17 13:53:16 +0200

    Initial commit; Added csproj;

---
+++ Negum.SDL/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Negum.SDL
+{
+    /// <summary>
+    /// </summary>
+    /// 
+    /// <author>
+    /// https://github.com/TheNegumProject/Negum.SDL
+    /// </author>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
